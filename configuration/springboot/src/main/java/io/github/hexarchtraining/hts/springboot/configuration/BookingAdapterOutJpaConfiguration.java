03fd3d6d55f933316a694a55e0bb044aeb913a6f
jtaterka <janusz.taterka@capgemini.com>
2021-09-29 20:58:42 +0200

    Small refactoring of a FindBookingByToken bci service

--- configuration/springboot/src/main/java/io/github/hexarchtraining/hts/springboot/configuration/BookingAdapterOutJpaConfiguration.java
+++ configuration/springboot/src/main/java/io/github/hexarchtraining/hts/springboot/configuration/BookingAdapterOutJpaConfiguration.java
@@ -64,9 +64,4 @@ public class BookingAdapterOutJpaConfiguration {
     public FindBookingsPort findBookingsPort() {
         return new FindBookinsJpaAdapter(bookingRepository);
     }
-
-    @Bean
-    public FindBookingByTokenUseCase findBookingByTokenUseCase() {
-        return new FindBookingByTokenJpaAdapter(bookingRepository);
-    }
 }
